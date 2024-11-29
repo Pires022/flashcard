59b8eb9bcdd406725a364e023021681d124292e4
Pires022 <00001104633255sp@al.educacao.sp.gov.br>
2024-11-29 09:11:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Qual foi a principal causa da Primeira Guerra Mundial?',
+    'A principal causa da Primeira Guerra Mundial foi o assassinato do arquiduque Francisco Ferdinando, herdeiro do trono austro-húngaro.'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é a estrela mais próxima da Terra?',
+    'A estrela mais próxima da Terra é o Sol.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior deserto do mundo?',
+    'O maior deserto do mundo é o Deserto do Saara.'
+)
+
+criaCartao(
+    'Anatomia',
+    'Quantos ossos tem o corpo humano adulto?',
+    'O corpo humano adulto tem, em média, 206 ossos.'
+)
